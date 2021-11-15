ac9b26389e5800995119701475edae4656461cda
Bryan Hann <nobody@nowhere>
2021-11-15 19:39:09 -0330

    Update vendor submodules.
    Use [$POET_VENDOR].
    
    Since the [.vendor] directory will rarely (if ever) be inspected,
    it is convenient to have it hidden. Hence the leading dot.

--- .insitu/activate.sh
+++ .insitu/activate.sh
@@ -4,6 +4,7 @@ export_ POET        $(dirname ${PWD})
 export_ POET_BIN    ${POET}/bin
 export_ POET_USER   ${POET}/user
 export_ POET_POEM   ${POET}/poem
-export_ PATH        ${PATH}:$POET_BIN
+export_ POET_VENDOR ${POET}/.vendor
 export_ PATH        ${POET_BIN}:$(./fixpath.py ${POET_BIN})
+
 . ${POET_USER}/dotfiles/dot$1
